85fc9809d422937fbbf38e5c764086801740175e
Mark Garner <mgarner@microsoft.com>
2026-02-07 17:49:28 -0600

    fix: register IBookService in DI container to resolve 500 errors

--- src/BookstoreApi/Program.cs
+++ src/BookstoreApi/Program.cs
@@ -1,8 +1,11 @@
+using BookstoreApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSingleton<IBookService, BookService>();
 
 var app = builder.Build();
 
