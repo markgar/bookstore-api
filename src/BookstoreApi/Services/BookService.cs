b57f05ff2058e34dec29cef1fa71bfdc83e91dab
Mark Garner <mgarner@microsoft.com>
2026-02-07 17:40:34 -0600

    Add BookService with thread-safe in-memory store

---
+++ src/BookstoreApi/Services/BookService.cs
@@ -0,0 +1,41 @@
+using System.Collections.Concurrent;
+using BookstoreApi.Models;
+
+namespace BookstoreApi.Services;
+
+/// <summary>
+/// In-memory implementation of <see cref="IBookService"/> using a thread-safe store.
+/// </summary>
+public class BookService : IBookService
+{
+    private readonly ConcurrentDictionary<int, Book> _books = new();
+    private int _nextId;
+
+    /// <inheritdoc />
+    public IEnumerable<Book> GetAll() => _books.Values;
+
+    /// <inheritdoc />
+    public Book? GetById(int id) => _books.GetValueOrDefault(id);
+
+    /// <inheritdoc />
+    public Book Add(Book book)
+    {
+        book.Id = Interlocked.Increment(ref _nextId);
+        _books[book.Id] = book;
+        return book;
+    }
+
+    /// <inheritdoc />
+    public bool Update(int id, Book book)
+    {
+        if (!_books.ContainsKey(id))
+            return false;
+
+        book.Id = id;
+        _books[id] = book;
+        return true;
+    }
+
+    /// <inheritdoc />
+    public bool Delete(int id) => _books.TryRemove(id, out _);
+}
